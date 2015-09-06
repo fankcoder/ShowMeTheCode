6f729a6431f341ced8b77c6b7a356ba4585d803f
icgoo <zf.ice@163.com>
2015-09-06 16:28:02 +0800

    s2

---
+++ s2/save_act.py
@@ -0,0 +1,13 @@
+import MySQLdb
+
+f = open('activation.txt','r')
+lines = f.readlines()
+f.close()
+conn = MySQLdb.connect(host='localhost',user='root',passwd='icgoozf',db='showmethecode')
+cursor = conn.cursor()
+for line in lines:
+    line = line.strip()
+    cursor.execute("""INSERT INTO activation(code) VALUES ('%s');""" %line)
+conn.commit()
+cursor.close()
+conn.close()
