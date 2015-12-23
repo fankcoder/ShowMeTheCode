1a9cd620cad05ac68ed53aba985de050c5413e77
fankcoder <zf.ice@163.com>
2015-12-23 22:22:05 +0800

    del

--- s24/todo/todolist/views.py
+++ s24/todo/todolist/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .forms import CreateList
 from .models import List
 
+
 # Create your views here.
 def index(request):
     form = CreateList()
+    
     if request.method=='POST':
         form = CreateList(request.POST)
         if form.is_valid():
@@ -14,5 +16,13 @@ def index(request):
             for each in  data:
                 print each.levelist[0]
             return render(request,'index.html',context)
+
+    elif 'del' in request.GET:
+        print request.GET['del']
+        id_num = request.GET['del']
+        List.objects.filter(id=id_num).delete()
+        data = List.objects.all()[::-1]
+        context = {'data':data}
+        return render(request,"index.html",context)
 
     return render(request,'index.html')
